a7e629db57ecc4604c4c24817aaaf57dff870445
Caio Andrade <caioertai@gmail.com>
2018-08-07 10:01:36 -0300

    Using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
-  # Read
-  # List all
-  #                    to: 'controller#action'
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/:id', to: 'restaurants#show' # params will have {id: {{A Restaurant ID}}
+  resources :restaurants
+  # # Read
+  # # List all
+  # #                    to: 'controller#action'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show' # params will have {id: {{A Restaurant ID}}
 
-  # Create
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
+  # # Create
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
 
-  # Update
-  get 'restaurants/:id/edit', to: 'restaurants#edit'
-  patch 'restaurants/:id', to: 'restaurants#update' # could be verb put
+  # # Update
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update' # could be verb put
 
-  # Destroy/Delete
-  delete 'restaurants/:id', to: 'restaurants#destroy'
+  # # Destroy/Delete
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 end
