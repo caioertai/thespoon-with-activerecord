f88ea50d1abac5c9b09daec9e79037ec117a19c3
Caio Andrade <caioertai@gmail.com>
2018-08-07 09:55:05 -0300

    Created the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read
+  # List all
+  #                    to: 'controller#action'
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/:id', to: 'restaurants#show' # params will have {id: {{A Restaurant ID}}
+
+  # Create
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  # Update
+  get 'restaurants/:id/edit', to: 'restaurants#edit'
+  patch 'restaurants/:id', to: 'restaurants#update' # could be verb put
+
+  # Destroy/Delete
+  delete 'restaurants/:id', to: 'restaurants#destroy'
 end
